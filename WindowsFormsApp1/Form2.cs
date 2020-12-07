2fbce9dae64d0f7c849ec3e965e0e44ec8c5919c
Clyde Francis Tome <tome.053122@calamba.sti.edu.ph>
2020-12-07 12:31:02 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterRoom rg = new RegisterRoom();
+            rg.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Amedities amed = new Amedities();
+            amed.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Form1 login = new Form1();
+            login.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RoomRates rr = new RoomRates();
+            rr.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DialogResult ex = MessageBox.Show("Are You Sure you want to Exit Application?","Warning",MessageBoxButtons.YesNo);
+            if (ex == DialogResult.Yes)
+            {
+                System.Environment.Exit(0);
+            }
+            
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/RegisterRoom.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class RegisterRoom : Form
+    {
+        public RegisterRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterRoom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Form2 mainform = new Form2();
+            mainform.Show();
+        }
+    }
+}
